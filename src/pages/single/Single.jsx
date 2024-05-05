2dfbce06a48a2566fb844334b3795513d8cdf9d4
Panji <alfapaji2012@gmail.com>
2024-05-05 21:52:26 +0700

    single

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -2,6 +2,7 @@ import "./single.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Chart from "../../components/chart/Chart";
+import List from "../../components/datatable/datatable";
 
 const Single = () => {
   return (
@@ -46,7 +47,10 @@ const Single = () => {
             <Chart aspect={3 / 1} type="order" />
           </div>
         </div>
-        <div className="bottom"></div>
+        <div className="bottom">
+          <h1 className="title">Last Transactions</h1>
+          <List />
+        </div>
       </div>
     </div>
   );
