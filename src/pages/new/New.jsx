646456f78d9d5aa9d56765178dd0e95eebb8bf20
Panji <alfapaji2012@gmail.com>
2024-05-05 23:01:22 +0700

    new

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -26,7 +26,28 @@ const New = ({ inputs, title }) => {
               alt=""
             />
           </div>
-          <div className="right">right</div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label htmlFor="file">
+                  Image: <DriveFolderUploadOutlinedIcon className="icon" />
+                </label>
+                <input
+                  type="file"
+                  id="file"
+                  onChange={(e) => setFile(e.target.files[0])}
+                  style={{ display: "none" }}
+                />
+              </div> 
+              {inputs.map((input) => (
+                <div className="formInput" key={input.id}>
+                  <label>{input.label}</label>
+                  <input type={input.type} placeholder={input.placeholder} />
+                </div>
+              ))}
+              <button>Send</button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
