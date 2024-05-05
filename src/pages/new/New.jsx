a2bed01fe936451535614f03d7699c06c3522f50
Panji <alfapaji2012@gmail.com>
2024-05-05 22:13:29 +0700

    new

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -2,13 +2,15 @@ import "./new.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 
-const New = () => {
+const New = ({ inputs, title }) => {
   return (
     <div className="new">
       <Sidebar />
       <div className="newContainer">
         <Navbar />
-        <div className="top">top</div>
+        <div className="top">
+          <h1>{title}</h1>
+        </div>
         <div className="bottom">
           <div className="left">left</div>
           <div className="right">right</div>
