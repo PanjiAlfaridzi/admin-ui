c4a2a56660b8e94ae9284b27a7fb8d3f8dfd6806
Panji <alfapaji2012@gmail.com>
2024-05-08 22:19:06 +0700

    main

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,62 +1,69 @@
-import "./sidebar.scss";
+import "./sidebar.scss"
 import { Link } from "react-router-dom";
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import PersonIcon from '@mui/icons-material/Person';
-import ProductionQuantityLimitsIcon from '@mui/icons-material/ProductionQuantityLimits';
-import AddBusinessIcon from '@mui/icons-material/AddBusiness';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import StoreIcon from '@mui/icons-material/Store';
+import PersonPinIcon from '@mui/icons-material/PersonPin';
 import LogoutIcon from '@mui/icons-material/Logout';
+import CategoryIcon from '@mui/icons-material/Category';
 
 const Sidebar = () => {
-  return (
-    <div className="sidebar">
-      <div className="top">
-        <Link to="/">
-          <span className="logo">Store</span>
-        </Link>
-      </div>
-      <hr />
-      <div className="center">
-        <ul>
-          <p className="title">MAIN</p>
-          <li>
-            <DashboardIcon className="icon" />
-            <span>Dashboard</span>
-          </li>
-          <p className="title">LIST</p>
-          <Link to="/users">
+    return (
+        <div className="sidebar">
+        <div className="top">
+          <Link to="/">
+            <span className="logo">Store</span>
+          </Link>
+        </div>
+        <hr />
+        <div className="center">
+          <ul>
+            <p className="title">MAIN</p>
             <li>
-              <PersonIcon className="user" />
-              <span>Users</span>
+                <DashboardIcon className="icon"/>
+              <span>Dashboard</span>
+            </li>
+            <p className="title">LIST</p>
+            <Link to="/users">
+              <li>
+              <PersonIcon className="icon"/>
+                <span>Users</span>
+              </li>
+            </Link>
+            <Link to="/products">
+              <li>
+              <InventoryIcon className="icon"/>
+                <span>Products</span>
+              </li>
+            </Link>
+            <li>
+            <StoreIcon className="icon"/>
+              <span>Orders</span>
+            </li>
+            <Link to="/categories">
+            <li>
+              <CategoryIcon className="icon" />
+              <span>Categories</span>
             </li>
           </Link>
-          <Link to="/products">
+            <p className="title">USER</p>
             <li>
-              <ProductionQuantityLimitsIcon className="product" />
-              <span>Products</span>
+            <PersonPinIcon className="icon"/>
+              <span>Profile</span>
             </li>
-          </Link>
-          <li>
-            <AddBusinessIcon className="orders" />
-            <span>Orders</span>
-          </li>
-          <p className="title">USER</p>
-          <li>
-            <AccountCircleIcon className="profile" />
-            <span>Profile</span>
-          </li>
-          <li>
-            <LogoutIcon className="logout" />
-            <span>Logout</span>
-          </li>
-        </ul>
+            <li>
+            <LogoutIcon className="icon"/>
+              <span>Logout</span>
+            </li>
+          </ul>
+        </div>
+        <div className="bottom">
+          <div className="colorOption"></div>
+          <div className="colorOption"></div>
+        </div>
       </div>
-      <div className="bottom">
-        <div className="colorOption"></div>
-        <div className="colorOption"></div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Sidebar;
